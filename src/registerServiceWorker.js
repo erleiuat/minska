66fab3ddad03d308f2290f382e39b1d8bdacfe13
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2019-01-11 13:10:10 +0100

    updates

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -19,7 +19,6 @@ if (process.env.NODE_ENV === 'production') {
         },
         updated () {
             console.log('New content is available: Please refresh.');
-            window.location.reload(true);
         },
         offline () {
             console.log('No internet connection found. App is running in offline mode.')
