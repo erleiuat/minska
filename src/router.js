5252dd0671b962c344d2f0238ef291bf8822e2d1
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2019-04-17 17:42:24 +0200

    history mode

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
     routes: [
         {
             path: '/dashboard',
