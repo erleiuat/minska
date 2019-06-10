68feef2feb71d973e95ac86c95bef59e6ed807cf
Elia <mail@eliareutlinger.ch>
2019-06-10 11:50:39 +0200

    redesign

--- src/views/Unsecure/Home.vue
+++ src/views/Unsecure/Home.vue
@@ -1,21 +1,23 @@
 <template>
     <v-container class="text-xs-center">
-        <v-layout row wrap>
-            <v-flex xs12>
+        <v-layout row wrap justify-center>
+            <v-flex xs10 md6>
                 <v-img :src="require('@/assets/minska_logo/title_color.png')" :lazy-src="require('@/assets/minska_logo/title_color_low.png')" aspect-ratio="1.7" contain></v-img>
+            </v-flex>
+            <v-flex xs12 md10>
                 <h1>{{ $t('title') }}</h1>
                 <p>{{ $t('subtitle') }}</p>
             </v-flex>
-            <v-flex xs12>
+            <v-flex xs12 md10>
                 <v-btn color="primary" block dark @click="$router.push('/login')">{{ $t('login') }}</v-btn>
             </v-flex>
-            <v-flex xs12>
+            <v-flex xs12 md10>
                 <v-btn color="primary" block dark outline @click="$router.push('/register')">{{ $t('register') }}</v-btn>
             </v-flex>
-            <v-flex xs12 class="pt-5">
+            <v-flex xs12 md10 class="pt-5">
                 <p>{{ $t('text') }}</p>
                 <v-divider></v-divider>
-                <v-img class="pa-5" :src="require('@/assets/demos/facts_de.png')" :lazy-src="require('@/assets/minska_logo/title_color_low.png')" contain></v-img>
+                <v-img :src="require('@/assets/demos/facts_de.png')" :lazy-src="require('@/assets/minska_logo/title_color_low.png')" contain></v-img>
                 <p>{{ $t('text2') }}</p>
                 <v-divider></v-divider>
                 <v-img class="pa-5" :src="require('@/assets/demos/weight.png')" :lazy-src="require('@/assets/minska_logo/title_color_low.png')" contain></v-img>
@@ -24,7 +26,7 @@
                 <v-img class="pa-5" :src="require('@/assets/demos/devices.png')" :lazy-src="require('@/assets/minska_logo/title_color_low.png')" contain></v-img>
                 <p>{{ $t('text4') }}</p>
             </v-flex>
-            <v-flex xs12>
+            <v-flex xs12 md10>
                 <v-btn color="primary" block dark @click="$router.push('/register')">{{ $t('register') }}</v-btn>
             </v-flex>
 
@@ -35,7 +37,7 @@
 <script>
 export default {
     name: 'Home',
-    
+
     i18n: {
         messages: {
             en: {
